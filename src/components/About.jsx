5e8a769c46808f30fe91490b73ba8d6670f0124b
sseezov <sseezov@mail.ru>
2022-04-29 14:18:41 +0700

    big update 29.04

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import MyPic from "../assets/UTgWXhK6jQg.jpg";
+import aboutStorage from "./aboutData";
 
 const About = () => {
+  const [toggleAbout, setToggleAbout] = useState(true);
+
   return (
     <div
       name="about"
@@ -14,26 +17,106 @@ const About = () => {
               О СЕБЕ
             </p>
           </div>
-          <div></div>
+          <div>
+            <button
+              onClick={() => {
+                setToggleAbout(!toggleAbout);
+              }}
+              className="text-4xl font-bold inline text-yellow-200 hover:text-yellow-400"
+            >
+              {toggleAbout ? ">" : "<"}
+            </button>
+          </div>
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 md:gap-8 md:px-4">
           <div className="sm:text-right">
             <img
-              className="md:float-right w-[160px] sm:w-[180px] md:w-[280px]"
+              className="ml-[20px] flex items-center justify-around md:float-right w-[160px] rounded-xl  sm:w-[180px] md:w-[280px]"
               src={MyPic}
               alt="это я"
             />
           </div>
+
           <div>
-            <p className="float-left text-base sm:text-base md:text-xl">
-              Мне 31 год, у меня 2 высших очных образования и аспирантура за
-              плечами. Восемь лет своей жизни я занимался наукой,
-              преимущественно философией, и преподаванием, но теперь
-              заинтересовался IT, хотя и здесь мне видится философия.
-              Программирование для меня - это демиургическая практика, где цифры
-              посредствуют логосу, а вечное солнце ноэматического открывается в
-              диджитализации, понимаемой как переоткрытие пифагорейских практик.
-            </p>
+            {toggleAbout ? (
+              <p className="float-left text-base sm:text-base md:text-xl">
+                Мне 31 год, у меня 2 высших очных образования и аспирантура за
+                плечами. Восемь лет своей жизни я занимался наукой,
+                преимущественно философией, и преподаванием, но теперь
+                заинтересовался IT, хотя и здесь мне видится философия.
+                Программирование для меня - это демиургическая практика, где
+                цифры посредствуют логосу, а вечное солнце ноэматического
+                открывается в диджитализации, понимаемой как переоткрытие
+                пифагорейских практик.
+              </p>
+            ) : (
+              <div>
+                <p className="float-left text-base sm:text-base md:text-xl pb-4">
+                  Я программист самоучка, прошел курсы верстки и js на{" "}
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-purple-300"
+                    href="https://www.freecodecamp.org/fcc0a4acf66-9b9c-4c1b-9064-83662a3c122a"
+                  >
+                    freecodecamp.org
+                  </a>
+                  ,{" "}
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-purple-300"
+                    href="https://www.theodinproject.com/"
+                  >
+                    theodinproject.com
+                  </a>
+                  , курсы по Реакту и Редаксу в{" "}
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-purple-300"
+                    href="https://www.youtube.com/channel/UCTW0FUhT0m-Bqg2trTbSs0g"
+                  >
+                    it-kamasutra{" "}
+                  </a>
+                  и вообще на ютубе (больше англоязычном) и по книгам. Решал
+                  всякие задачки на{" "}
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-purple-300"
+                    href="https://www.codewars.com/users/sseezov"
+                  >
+                    codewars
+                  </a>{" "}
+                  и{" "}
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-purple-300"
+                    href="https://js.checkio.org/user/Sergej_Sizov/"
+                  >
+                    ts.checkio
+                  </a>
+                  . Читал книги, делал проекты, et cetera. Вот мой{" "}
+                  <a
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-purple-300"
+                    href="https://github.com/sseezov"
+                  >
+                    гит
+                  </a>
+                  .
+                </p>
+                <p className="float-left text-base sm:text-base md:text-xl">
+                  Меня интересует, прежде всего, возможность быстрого роста как
+                  специалиста, а потому я ищу мощную команду, чтобы{" "}
+                  <strong className="text-red-600">дичайше</strong>, например,
+                  прокачаться.
+                </p>
+              </div>
+            )}
           </div>
         </div>
       </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,106 +1,68 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import workStorage from "./workData";
 
 const Work = () => {
+  const [index, setIndex] = useState(0);
+  const [workData, setWorkData] = useState(workStorage);
+
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 flex justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-yellow-400">
-            ОПЫТ РАБОТЫ
+            ОПЫТ РАБОТЫ И ПРОЕКТЫ
           </p>
-          <p className="text-base md:text-xl py-6">
-            {" "}
-            Мои проекты вы можете посмотреть на{" "}
-            <a
-              rel="noreferrer"
-              target="_blank"
-              className="text-purple-300"
-              href="https://github.com/sseezov"
-            >
-              гитхабе
-            </a>
-            . Я занимался созданием{" "}
-            <a
-              rel="noreferrer"
-              target="_blank"
-              className="text-purple-300"
-              href="https://github.com/sseezov/codepit/tree/main/react%20samurai%20%E2%80%94%20%D0%BA%D0%BE%D0%BF%D0%B8%D1%8F/my-app"
-            >
-              социальной сети
-            </a>
-            , написал{" "}
-            <a
-              rel="noreferrer"
-              target="_blank"
-              className="text-purple-300"
-              href="https://github.com/sseezov/codepit/tree/main/weather%20cli"
-            >
-              CLI
-            </a>
-            , которая выводит данные о погоде, в зависимости от полученных
-            данных, написал{" "}
-            <a
-              rel="noreferrer"
-              target="_blank"
-              className="text-purple-300"
-              href="https://github.com/sseezov/churchsite"
-            >
-              лендинговую страницу для храма
-            </a>{" "}
-            и этот{" "}
-            <a
-              rel="noreferrer"
-              target="_blank"
-              className="text-purple-300"
-              href="https://github.com/sseezov/done"
-            >
-              сайт-портфолио
-            </a>
-            . Остальное, а это большая часть написанного кода - практика в
-            создании отдельных интерфейсов и алгоритмов. В ходе учебы я закончил
-            множество практических модулей на{" "}
-            <a
-              rel="noreferrer"
-              target="_blank"
-              className="text-purple-300"
-              href="https://www.freecodecamp.org/fcc0a4acf66-9b9c-4c1b-9064-83662a3c122a"
-            >
-              freecodecamp.org
-            </a>
-            , модули{" "}
-            <a
-              rel="noreferrer"
-              target="_blank"
-              className="text-purple-300"
-              href="https://www.theodinproject.com/"
-            >
-              theodinproject.com
-            </a>
-            , задачи на{" "}
-            <a
-              rel="noreferrer"
-              target="_blank"
-              className="text-purple-300"
-              href="https://js.checkio.org/user/Sergej_Sizov/"
-            >
-              ts.checkio
-            </a>
-            ,{" "}
-            <a
-              rel="noreferrer"
-              target="_blank"
-              className="text-purple-300"
-              href="https://www.codewars.com/users/sseezov"
-            >
-              codewars
-            </a>{" "}
-            и т.п.
-            <br />
-          </p>
-          <p className="text-base sm:text-base md:text-xl text-purple-600">
-            А еще я свободно владею английским языком и имею отличный набор
-            софт-скиллов.
-          </p>
+
+          <div className="p-8 md:min-h-[370px] md:min-w-[1200px] sm:flex-col">
+            <h1 className="text-xl md:text-2xl font-bold inline text-gray-300">
+              {workData[index].name}
+            </h1>
+            <div className="md:flex md:flex-row">
+              <img
+                className="ml-[20px] max-h-[200px] md:max-h-[250px] rounded-xl p-2"
+                src={workData[index].image}
+                alt={workData[index].name}
+              />
+              <p className="text-base md:text-xl text-gray-300 md:p-2">
+                {workData[index].text}
+              </p>
+            </div>
+          </div>
+          <div className="md:p-2 flex items-center justify-around">
+            {index > 0 ? (
+              <button
+                className="text-yellow-200 hover:text-yellow-400 font-bold text-6xl"
+                onClick={() => {
+                  setIndex(index - 1);
+                }}
+              >
+                {"<"}
+              </button>
+            ) : (
+              <button className="text-[#0a192f] font-bold text-6xl">
+                {"<"}
+              </button>
+            )}
+            <div className="font-bold text-gray-300 text-xl">{`${
+              index + 1
+            } страница`}</div>
+
+            {index < workData.length - 1 ? (
+              <button
+                className="text-yellow-200 hover:text-yellow-400 font-bold text-6xl"
+                onClick={() => {
+                  setIndex(index + 1);
+                }}
+              >
+                {">"}
+              </button>
+            ) : (
+              <button className="text-[#0a192f] font-bold text-6xl">
+                {">"}
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
