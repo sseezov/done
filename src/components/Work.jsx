5663e2ecb88d547f6a3f61ff0a63ffdd0e7b34a6
sseezov <sseezov@mail.ru>
2022-05-04 10:25:44 +0700

    vertical-align work fix 2

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,8 +6,11 @@ const Work = () => {
   const [workData, setWorkData] = useState(workStorage);
 
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
-      <div className="max-w-[1000px] mx-auto px-4 flex items-center justify-center w-full h-full ">
+    <div
+      name="work"
+      className="w-full pt-4 md:h-screen text-gray-300 bg-[#0a192f]"
+    >
+      <div className="max-w-[1000px] m-auto p-4 flex items-center justify-center w-full h-full ">
         <div className="ml-[33px]">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-yellow-400">
             ОПЫТ РАБОТЫ И ПРОЕКТЫ
